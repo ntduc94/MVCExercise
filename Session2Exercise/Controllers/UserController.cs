18b6d2acdf8c268813fdbc70eafa47ae202d8bc7
nguyen tan duc <ntanduc94@outlook.com>
2018-07-22 14:26:28 +0700

    Add project files.

---
+++ Session2Exercise/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using ExerciseMVC01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExerciseMVC01.Controllers
+{
+    public class UserController : Controller
+    {
+        private List<User> Users = new List<User>
+        {
+            new User { Username = "user 1", Email = "email 1", Phone = "phone 1", Age = 1 },
+            new User { Username = "user 2", Email = "email 2", Phone = "phone 2", Age = 2 },
+            new User { Username = "user 3", Email = "email 3", Phone = "phone 3", Age = 3 }
+        };
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(string username, string email, string phone, int age)
+        {
+            Users.Add(new User { Username = username, Email = email, Phone = phone, Age = age });
+            //ViewBag.User = new User { Username = username, Email = email, Phone = phone, Age = age };
+            return Json(Users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Session2Exercise/Models/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExerciseMVC01.Models
+{
+    public class UserRepository
+    {
+        public List<User> Users { get; set; }
+
+        //public List<User> Retrieve(int username)
+        //{
+
+        //}
+
+        public void Save(User user)
+        {
+            Users.Add(user);
+        }
+    }
+}
